c54ee88ca527057d46978b0044fd9fe7cccb2734
Anderson Ryan <santoryan1408@gmail.com>
2022-03-16 20:42:55 -0300

    Added bush colision

--- pokegame.js
+++ pokegame.js
@@ -2,13 +2,16 @@ let canvas = document.querySelector('#canvas');
 let ctx = canvas.getContext('2d');
 let p1 = {
     x:20, y:20,
-    w:20, h:12, color: 'orange'
+    w:40, h:40, color: 'red'
 }
-let player = document.createElement('img');
 let positions = {
     cima:119, baixo:115,
     esquerda:97, direita:100
 }
+
+let bush = [{x: 0, y:70,w: 400, h:250},
+            {x: 500, y:70,w: 400, h:250}
+]
 
 function keypressPlayer(event){
     let key = event.keyCode; // w = 119, a = 97, s = 115, d = 100
@@ -27,16 +30,32 @@ function keypressPlayer(event){
 
 function loop(){
     window.requestAnimationFrame(loop, canvas);
-    render();
+    render(); colide();
 }
 
 function render(){
-    //Objeto de colisão
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.fillStyle = p1.color = 'red';
-    ctx.fillRect(p1.x, p1.y, 20, 20);
+    //bush
+    ctx.fillStyle = '#fff';
+    ctx.fillRect(bush[0].x, bush[0].y, bush[0].w, bush[0].h)
+    ctx.fillStyle = '#909090'
+    ctx.fillRect(bush[1].x, bush[1].y, bush[1].w, bush[1].h)
+    //Player
+    ctx.fillStyle = p1.color;
+    ctx.fillRect(p1.x, p1.y, p1.w, p1.h);
 
 }
 
+function colide(){
+    if((p1.x + p1.w > bush[0].x && p1.x < bush[0].x + bush[0].w &&
+        p1.y + p1.h > bush[0].y && p1.y < bush[0].y + bush[0].h) || 
+        p1.x + p1.w > bush[1].x && p1.x < bush[1].x + bush[1].w &&
+        p1.y + p1.h > bush[1].y && p1.y < bush[1].y + bush[1].h){
+
+            p1.color = '#000'
+    }else{
+        p1.color = 'red'
+    }
+}
 window.addEventListener('keypress', keypressPlayer);
 loop()
